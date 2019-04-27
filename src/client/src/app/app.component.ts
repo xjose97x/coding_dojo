f791f2cd459b61a9a580a13d1ff0f11dbff253af
xjose97x <joseignacioescudero97@hotmail.com>
2019-04-27 13:28:49 -0500

    added loading logic

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { AppState } from './core/state/app.state';
+import { Observable, Subscription } from 'rxjs';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'idukay';
+  @Select(AppState.loading) processLoading$: Observable<boolean>;
+  routeLoading = false;
+
+  routerSubscription: Subscription;
+
+  constructor(private router: Router, private cd: ChangeDetectorRef) {
+    this.routerSubscription = this.router.events
+    .subscribe(
+      (event: Event) => {
+        if (event instanceof NavigationStart) {
+          this.routeLoading = true;
+        } else if (event instanceof NavigationEnd
+          || event instanceof NavigationCancel
+          || event instanceof NavigationError) {
+          this.routeLoading = false;
+        }
+        this.cd.detectChanges();
+      },
+      error => {
+        this.toastService.error(error);
+      }
+    );
+  }
 }
