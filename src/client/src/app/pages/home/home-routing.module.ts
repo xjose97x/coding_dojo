24a31291e72bb8a9b4473d3c842906179d9bc074
xjose97x <joseignacioescudero97@hotmail.com>
2019-04-27 14:56:20 -0500

    created rooms component

--- src/client/src/app/pages/home/home-routing.module.ts
+++ src/client/src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'rooms',
+    component: RoomsComponent
   }
 ];
 
--- src/client/src/app/pages/home/home.module.ts
+++ src/client/src/app/pages/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { RoomsComponent } from './pages/rooms/rooms.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, RoomsComponent]
 })
 export class HomeModule { }
