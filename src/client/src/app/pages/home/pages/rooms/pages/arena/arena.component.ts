bbdfc9ce97459c7fde90d3e20e7c5c1373590b64
xjose97x <joseignacioescudero97@hotmail.com>
2019-04-27 16:17:34 -0500

    arena room

--- src/client/src/app/pages/home/pages/rooms/pages/arena/arena.component.ts
+++ src/client/src/app/pages/home/pages/rooms/pages/arena/arena.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RoomService } from 'src/app/core/services/room.service';
+import { Room } from 'src/app/shared/models/room.model';
+import { Store } from '@ngxs/store';
+import { AppState } from 'src/app/core/state/app.state';
 
 @Component({
   selector: 'app-arena',
@@ -8,11 +12,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArenaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  room: Room;
+
+  constructor(
+    private route: ActivatedRoute,
+    private roomService: RoomService,
+    private store: Store) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      console.log(params['id']);
+      this.roomService.getRoomById(params['id']).subscribe(room => this.room = room);
     });
+  }
+
+  isCreator(): boolean {
+    return this.store.selectSnapshot<string>(AppState.email) === this.room.creator.email;
+  }
+
+  startMeeting() {
+    return alert('Meeting started by Creator!');
   }
 }
