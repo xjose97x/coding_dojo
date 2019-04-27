98181c7955f22fd7fa090e27fbf286e4a08a50ab
xjose97x <joseignacioescudero97@hotmail.com>
2019-04-27 13:48:14 -0500

    auth guards

---
+++ src/client/src/app/core/guards/non-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../state/app.state';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NonAuthGuard implements CanActivate {
+  constructor(public router: Router, private store: Store) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      const token = this.store.selectSnapshot(AppState.token);
+      if (token) {
+        this.router.navigate(['/app']);
+        return false;
+      }
+      return true;
+  }
+}
