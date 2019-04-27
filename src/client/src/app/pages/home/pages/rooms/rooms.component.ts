d578b87316d0054350ecf2e7ea4ed0637a3cfc2c
xjose97x <joseignacioescudero97@hotmail.com>
2019-04-27 16:02:30 -0500

    show if user is creator of room

--- src/client/src/app/pages/home/pages/rooms/rooms.component.ts
+++ src/client/src/app/pages/home/pages/rooms/rooms.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomService } from 'src/app/core/services/room.service';
 import { Room } from 'src/app/shared/models/room.model';
+import { Store } from '@ngxs/store';
+import { AppState } from 'src/app/core/state/app.state';
 
 @Component({
   selector: 'app-rooms',
@@ -9,14 +11,18 @@ import { Room } from 'src/app/shared/models/room.model';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor(private roomService: RoomService) { }
+  constructor(private roomService: RoomService, private store: Store) { }
 
   rooms: Room[];
 
   ngOnInit() {
     this.roomService.getAllRooms().subscribe(r => {
       this.rooms = r;
-    })
+    });
+  }
+
+  canEdit(room: Room): boolean {
+    return this.store.selectSnapshot<string>(AppState.email) === room.creator.email;
   }
 
 }
